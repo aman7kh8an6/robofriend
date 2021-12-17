5f990329a11ba7c96c3fd5002ff1459bfa31f859
aman7kh8an6 <aman7kh8an6@gmail.com>
2021-12-17 15:28:34 +0530

    Updates

---
+++ precache-manifest.6beea3263fc7693be81637811f69df77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c99e7b734b22ea46f32",
+    "url": "/robofriend/static/css/main.5caf6800.chunk.css"
+  },
+  {
+    "revision": "3c99e7b734b22ea46f32",
+    "url": "/robofriend/static/js/main.3c99e7b7.chunk.js"
+  },
+  {
+    "revision": "88c3db276184fec9b66a",
+    "url": "/robofriend/static/js/1.88c3db27.chunk.js"
+  },
+  {
+    "revision": "17ab0b35cc6f0fe9ec2a",
+    "url": "/robofriend/static/css/2.dd903819.chunk.css"
+  },
+  {
+    "revision": "17ab0b35cc6f0fe9ec2a",
+    "url": "/robofriend/static/js/2.17ab0b35.chunk.js"
+  },
+  {
+    "revision": "10f6012eee6bd10d95d3",
+    "url": "/robofriend/static/js/runtime~main.10f6012e.js"
+  },
+  {
+    "revision": "848d63901f72bd91cc2811b6d9e0337c",
+    "url": "/robofriend/index.html"
+  }
+];
